9cbbd645015819982831693f775333e8ec14fe89
David Tran <109535790+david125tran@users.noreply.github.com>
2023-03-03 22:03:30 -0500

    Update desktop_app.py

--- desktop_app.py
+++ desktop_app.py
@@ -26,15 +26,17 @@ window.config(padx=50, pady=50, bg=WHITE)
 canvas = Canvas(width=200, height=300, bg=WHITE, highlightthickness=0)
 logo_img = PhotoImage(file="logo.png")
 
-# Add an image later
+# Add audiobook image
 canvas.create_image(100, 150, image=logo_img)
 canvas.grid(column=1, row=0)
 
 #--------------------------------------Select File Button-------------------------------------#
-filename = ""
+filename = "" # The variable filename is declared and will be changed once the user selects a file
+
 def select_file():
+    '''This function allows the user to open the file explorer to select a pdf file'''
+
     global filename
-    '''Call this function when the user opens a file.'''
     filetypes = (
         ('pdf files', '*.pdf'),
     )
@@ -47,6 +49,8 @@ def select_file():
     info_label.config(text=filename)
 
 def submit_button_clicked():
+    '''When the submit button is clicked, the selected pdf file then gets converted into mp3 audio file'''
+
     # --------------------------------------Open The PDF-------------------------------------#
     pdf = open(filename, 'rb')
     pdf_reader = PyPDF2.PdfFileReader(pdf)
@@ -57,12 +61,12 @@ def submit_button_clicked():
     # -----------------------------------Extract The Text------------------------------------#
     text = ""
     for page_number in range(0, total_pages):
-    # Extract the text from each page
+        # Extract the text from each page
         new_page = pdf_reader.getPage(page_number)
         new_page_text = new_page.extractText()
         text = text + "\n\n" + new_page_text   # Add the text to the previous pages if there are any
 
-    # Convert all of the text to speech (.mp3 file)
+    # -----------------------------------Convert the text to mp3 audio------------------------------------#
     audio = gTTS(text=text, lang='en', slow=False)
     # Each page becomes a new file
     audio_file_name = f"{audio_name.get()}.mp3"
@@ -72,7 +76,7 @@ def submit_button_clicked():
     os.system("audio.mp3")
     print(text)
 
-# -----------------------------------File Explorer Button------------------------------------#
+# -----------------------------------Buttons------------------------------------#
 file_explorer_button = Button(text="Open", highlightthickness=0, width=14, command=select_file)
 file_explorer_button.grid(column=1, row=2)
 
@@ -82,11 +86,14 @@ submit_button.grid(column=1, row=3)
 # -----------------------------------Labels------------------------------------#
 info_label = Label(text="Enter the file name", fg=BLACK, bg=WHITE, font=(FONT_NAME, 12))
 info_label.grid(column=1, row=1)
+
 mp3_label = Label(text=".mp3", fg=BLACK, bg=WHITE, font=(FONT_NAME, 12))
 mp3_label.grid(column=2, row=5)
 
 audio_name_label = Label(text="Enter a name for the new audio:", fg=BLACK, bg=WHITE, font=(FONT_NAME, 12))
 audio_name_label.grid(column=1, row=4)
+
+# -----------------------------------Entries------------------------------------#
 audio_name = Entry(fg=BLACK, bg=WHITE, font=(FONT_NAME, 12))
 audio_name.grid(column=1, row=5)
 
